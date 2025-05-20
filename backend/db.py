a1d4ea691c7991446c1c7f2750b39604f538a954
Rahul-1462 <gagireddy2002@gmail.com>
2025-05-20 18:03:40 +0530

    Delete backend directory

--- backend/db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-# Connect to MongoDB
-# If using MongoDB Atlas, replace with your connection string
-client = MongoClient('mongodb://localhost:27017/')
-db = client.blog_database
